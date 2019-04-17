57a74d549406ea1a1e7fe594e81582cb4e64770f
Manolo Perez <31350932+Manolomon@users.noreply.github.com>
2019-04-17 17:38:07 -0500

    Update pipe_runner.go

--- pipe_runner.go
+++ pipe_runner.go
@@ -1,5 +1,7 @@
 package main
 
+// Se agregan módulos principales para la ejecución
+// de comandeos del sistema opertivo
 import (
     "fmt"
     "os"
@@ -7,22 +9,31 @@ import (
 )
 
 func main() {
+    // Lectura de archivo de dónde se buscarán los correos
     fmt.Print("Enter input filename: ")
         var inputfile string    
         fmt.Scanln(&inputfile)
+    // Lectura del nombre del archivo de output del resultado
     fmt.Print("Enter output filename: ")
         var outputfile string
         fmt.Scanln(&outputfile)
-
+    
+    // Ejecución de los comandos para cada proceso
     c1 := exec.Command("python", "log_split.py", inputfile)
     c2 := exec.Command("ruby", "mail_filter.rb")
     c3 := exec.Command("java", "EmailWriter", outputfile)
-    c2.Stdin, _ = c1.StdoutPipe()
-    c3.Stdin, _ = c2.StdoutPipe()
-    c3.Stdout = os.Stdout
+    
+    // Asociasión a través de Pipes de Golang
+    c2.Stdin, _ = c1.StdoutPipe() // El out del primer proceso será el in del segundo
+    c3.Stdin, _ = c2.StdoutPipe() // El out del segundo proceso será el in del tercero
+    c3.Stdout = os.Stdout // El out del tercer proceso se muestra en consola
+    
+    // Ejecución
     _ = c3.Start()
     _ = c2.Start()
     _ = c1.Run()
+    
+    // En espera
     _ = c2.Wait()
     _ = c3.Wait()
 }
